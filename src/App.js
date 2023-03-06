911755a708c89d36ee39b62e5001893906720d45
Usman Developer <thedeveloperusman@gmail.com>
2023-03-06 17:47:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Usman develoiper</code> and save to reload.
+          Testing <code> For AWS Deployment </code>
         </p>
         <a
           className="App-link"
